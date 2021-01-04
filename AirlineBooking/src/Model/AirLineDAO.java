7765fdf9d6be4762917b09751d871d6497a059ba
hanzzb <hellojione2@gmail.com>
2021-01-04 22:40:23 +0900

    주석 추가

--- AirlineBooking/src/Model/AirLineDAO.java
+++ AirlineBooking/src/Model/AirLineDAO.java
@@ -10,220 +10,207 @@ import java.util.Vector;
 
 public class AirLineDAO {
 
-	//String jdbcDriver = "com.mysql.cj.jdbc.Driver";
-	String jdbcDriver = "com.mysql.jdbc.Driver";
-	String jdbcUrl = "jdbc:mysql://localhost:3306/airplanereservation?&serverTimezone=Asia/Seoul&useSSL=false";
-	Connection conn;
+   // String jdbcDriver = "com.mysql.cj.jdbc.Driver";
+   String jdbcDriver = "com.mysql.jdbc.Driver";
+   String jdbcUrl = "jdbc:mysql://localhost:3306/airplanereservation?&serverTimezone=Asia/Seoul&useSSL=false";
+   Connection conn;
 
-	PreparedStatement pstmt;
-	ResultSet rs;
+   PreparedStatement pstmt;
+   ResultSet rs;
 
-	Vector<String> items = null;
-	String sql;
+   Vector<String> items = null;
+   String sql;
 
-	public AirLineDAO() {
-		connectDB();
-	}
+   public AirLineDAO() {
+      connectDB();
+   }
 
-	public void connectDB() {
-		try {
-			Class.forName(jdbcDriver);
+   public void connectDB() {
+      try {
+         Class.forName(jdbcDriver);
 
-			conn = DriverManager.getConnection(jdbcUrl, "root", "0000");
-			if (conn == null)
-				System.out.println("conn is null");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+         conn = DriverManager.getConnection(jdbcUrl, "root", "0000");
+         if (conn == null)
+            System.out.println("conn is null");
+      } catch (Exception e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+   }
 
-	public void closeDB() {
-		try {
-			pstmt.close();
-			rs.close();
-			conn.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+   public void closeDB() {
+      try {
+         pstmt.close();
+         rs.close();
+         conn.close();
+      } catch (SQLException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+   }
 
-	// 전체 가져오기
-	public ArrayList<AirLine> getAllALInfo() throws SQLException {
-		// connectDB();
-		sql = "select * from airlineinfo";
+   // 전체 항공 정보를 가져오는 메소드
+   public ArrayList<AirLine> getAllALInfo() throws SQLException {
+      // connectDB();
+      sql = "select * from airlineinfo";
 
-		ArrayList<AirLine> datas = new ArrayList<AirLine>();
-		pstmt = conn.prepareStatement(sql);
-		rs = pstmt.executeQuery();
+      ArrayList<AirLine> datas = new ArrayList<AirLine>();
+      pstmt = conn.prepareStatement(sql);
+      rs = pstmt.executeQuery();
 
-		while (rs.next()) {
-			AirLine p = new AirLine();
-			p.setID(rs.getInt("ID"));
-			p.setAirLineNm(rs.getString("airLineNm"));
-			p.setArrAirportNm(rs.getString("arrAirportNm"));
-			p.setArrPlandTime(rs.getString("arrPlandTime"));
-			p.setDepAirportNm(rs.getString("depAirportNm"));
-			p.setDepPlandTime(rs.getString("depPlandTime"));
-			p.setEconomyCharge(rs.getInt("economyCharge"));
-			p.setPrestigeCharge(rs.getInt("prestigeCharge"));
-			datas.add(p);
-			// throws SQLException
-		}
+      while (rs.next()) {
+         AirLine p = new AirLine();
+         p.setID(rs.getInt("ID"));
+         p.setAirLineNm(rs.getString("airLineNm"));
+         p.setArrAirportNm(rs.getString("arrAirportNm"));
+         p.setArrPlandTime(rs.getString("arrPlandTime"));
+         p.setDepAirportNm(rs.getString("depAirportNm"));
+         p.setDepPlandTime(rs.getString("depPlandTime"));
+         p.setEconomyCharge(rs.getInt("economyCharge"));
+         p.setPrestigeCharge(rs.getInt("prestigeCharge"));
+         datas.add(p);
+      }
 
-		return datas;
-	}
+      return datas;
+   }
 
-	// ID에 해당하는 정보 가져오기
-	public AirLine getALInfo(int ID) {
-		try {
-			sql = "select * from airlineinfo where ID = ? ";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, String.valueOf(ID));
-			rs = pstmt.executeQuery();
-			
-			AirLine p = new AirLine();
-			rs.next();
-			p.setID(rs.getInt("ID"));
-			p.setAirLineNm(rs.getString("airLineNm"));
-			p.setArrAirportNm(rs.getString("arrAirportNm"));
-			p.setArrPlandTime(rs.getString("arrPlandTime"));
-			p.setDepAirportNm(rs.getString("depAirportNm"));
-			p.setDepPlandTime(rs.getString("depPlandTime"));
-			p.setEconomyCharge(rs.getInt("economyCharge"));
-			p.setPrestigeCharge(rs.getInt("prestigeCharge"));
-			return p;
+   // ID에 해당하는 항공 정보 가져오는 메소드
+   public AirLine getALInfo(int ID) {
+      try {
+         sql = "select * from airlineinfo where ID = ? ";
+         pstmt = conn.prepareStatement(sql);
+         pstmt.setString(1, String.valueOf(ID));
+         rs = pstmt.executeQuery();
 
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+         AirLine p = new AirLine();
+         rs.next();
+         p.setID(rs.getInt("ID"));
+         p.setAirLineNm(rs.getString("airLineNm"));
+         p.setArrAirportNm(rs.getString("arrAirportNm"));
+         p.setArrPlandTime(rs.getString("arrPlandTime"));
+         p.setDepAirportNm(rs.getString("depAirportNm"));
+         p.setDepPlandTime(rs.getString("depPlandTime"));
+         p.setEconomyCharge(rs.getInt("economyCharge"));
+         p.setPrestigeCharge(rs.getInt("prestigeCharge"));
+         return p;
 
-		return null;
-	}
-	
-	public ArrayList<AirLine> getALInfoByChoice(String depAirportNm, String arrAirportNm, String date) throws SQLException {
-		// connectDB();
-		sql = "select * from airlineinfo where arrAirportNm = ? and depAirportNm = ? and depPlandTime like ?";
-		
-		ArrayList<AirLine> datas = new ArrayList<AirLine>();
-		pstmt = conn.prepareStatement(sql);
-		pstmt.setString(1, arrAirportNm);
-		pstmt.setString(2, depAirportNm);
-		pstmt.setString(3, date+"%");
-		System.out.println(pstmt);
-		rs = pstmt.executeQuery();
+      } catch (SQLException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
 
-		while (rs.next()) {
-			AirLine p = new AirLine();
-			p.setID(rs.getInt("ID"));
-			p.setAirLineNm(rs.getString("airLineNm"));
-			p.setArrAirportNm(rs.getString("arrAirportNm"));
-			p.setArrPlandTime(rs.getString("arrPlandTime"));
-			p.setDepAirportNm(rs.getString("depAirportNm"));
-			p.setDepPlandTime(rs.getString("depPlandTime"));
-			p.setEconomyCharge(rs.getInt("economyCharge"));
-			p.setPrestigeCharge(rs.getInt("prestigeCharge"));
-			datas.add(p);
-			// throws SQLException
-		}
+      return null;
+   }
 
-		return datas;
-	}
-	
-	public Vector<String> getItems() {
-		Vector<String> arr = new Vector<String>();
-		try {
-			sql = "select * from airlineinfo";
-			pstmt = conn.prepareStatement(sql);
+   // 사용자가 입력한 정보를 바탕으로 항공 정보를 가져오는 메소드
+   public ArrayList<AirLine> getALInfoByChoice(String depAirportNm, String arrAirportNm, String date)
+         throws SQLException {
+      sql = "select * from airlineinfo where arrAirportNm = ? and depAirportNm = ? and depPlandTime like ?";
 
-			ResultSet sucess = pstmt.executeQuery();
-			while (sucess.next()) {
-				arr.add(sucess.getString("ID"));
-			}
-			return arr;
-		} catch (SQLException e) {
-		}
-		return arr;
-	}
+      ArrayList<AirLine> datas = new ArrayList<AirLine>();
+      pstmt = conn.prepareStatement(sql);
+      pstmt.setString(1, arrAirportNm);
+      pstmt.setString(2, depAirportNm);
+      pstmt.setString(3, date + "%");
+      System.out.println(pstmt);
+      rs = pstmt.executeQuery();
 
-	public int updateALInfo(AirLine p) {
-		sql = "UPDATE airlineinfo SET arrPlandTime = ?, depPlandTime = ?, economyCharge = ?, prestigeCharge = ? WHERE ID = ?";
-		try {
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, p.getArrPlandTime());
-			pstmt.setString(2, String.valueOf(p.getDepPlandTime()));
-			pstmt.setString(3, String.valueOf(p.getEconomyCharge()));
-			pstmt.setString(4, String.valueOf(p.getPrestigeCharge()));
-			pstmt.setString(5, String.valueOf(p.getID()));
-			System.out.println(pstmt);
+      while (rs.next()) {
+         AirLine p = new AirLine();
+         p.setID(rs.getInt("ID"));
+         p.setAirLineNm(rs.getString("airLineNm"));
+         p.setArrAirportNm(rs.getString("arrAirportNm"));
+         p.setArrPlandTime(rs.getString("arrPlandTime"));
+         p.setDepAirportNm(rs.getString("depAirportNm"));
+         p.setDepPlandTime(rs.getString("depPlandTime"));
+         p.setEconomyCharge(rs.getInt("economyCharge"));
+         p.setPrestigeCharge(rs.getInt("prestigeCharge"));
+         datas.add(p);
+         // throws SQLException
+      }
 
-			int r = pstmt.executeUpdate();
-			if (r > 0)
-				System.out.println(p.getID() + "update success");
-			else
-				System.out.println(p.getID() + " update fail");
+      return datas;
+   }
 
-			return r;
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return 0;
-	}
+   // 항공 정보 업데이트를 수행하는 메소드
+   public int updateALInfo(AirLine p) {
+      sql = "UPDATE airlineinfo SET arrPlandTime = ?, depPlandTime = ?, economyCharge = ?, prestigeCharge = ? WHERE ID = ?";
+      try {
+         pstmt = conn.prepareStatement(sql);
+         pstmt.setString(1, p.getArrPlandTime());
+         pstmt.setString(2, String.valueOf(p.getDepPlandTime()));
+         pstmt.setString(3, String.valueOf(p.getEconomyCharge()));
+         pstmt.setString(4, String.valueOf(p.getPrestigeCharge()));
+         pstmt.setString(5, String.valueOf(p.getID()));
+         System.out.println(pstmt);
 
-	public int addALInfo(AirLine p) {
+         int r = pstmt.executeUpdate();
+         if (r > 0)
+            System.out.println(p.getID() + "update success");
+         else
+            System.out.println(p.getID() + " update fail");
 
-		try {
-			sql = "insert into airlineinfo values(0, ?, ?, ?, ?, ?, ?, ?)";
-			pstmt = conn.prepareStatement(sql);
+         return r;
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }
+      return 0;
+   }
 
-			pstmt.setString(1, p.getAirLineNm());
-			pstmt.setString(2, p.getArrAirportNm());
-			pstmt.setString(3, p.getArrPlandTime());
-			pstmt.setString(4, p.getDepAirportNm());
-			pstmt.setString(5, p.getDepPlandTime());
-			pstmt.setInt(6, p.getEconomyCharge());
-			pstmt.setInt(7, p.getPrestigeCharge());
+   // 항공 추가를 수행하는 메소드
+   public int addALInfo(AirLine p) {
 
-			int r = pstmt.executeUpdate();
-			if (r > 0) {
-				System.out.println(p.getAirLineNm() + " insert success");
-			}
+      try {
+         sql = "insert into airlineinfo values(0, ?, ?, ?, ?, ?, ?, ?)";
+         pstmt = conn.prepareStatement(sql);
 
-			else
-				System.out.println(p.getAirLineNm() + " insert failure");
+         pstmt.setString(1, p.getAirLineNm());
+         pstmt.setString(2, p.getArrAirportNm());
+         pstmt.setString(3, p.getArrPlandTime());
+         pstmt.setString(4, p.getDepAirportNm());
+         pstmt.setString(5, p.getDepPlandTime());
+         pstmt.setInt(6, p.getEconomyCharge());
+         pstmt.setInt(7, p.getPrestigeCharge());
 
-			return r;
+         int r = pstmt.executeUpdate();
+         if (r > 0) {
+            System.out.println(p.getAirLineNm() + " insert success");
+         }
 
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return 0;
-	}
+         else
+            System.out.println(p.getAirLineNm() + " insert failure");
 
-	public int delALInfo(int ID) {
+         return r;
 
-		try {
-			sql = "delete from airlineinfo where ID = ?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, ID);
+      } catch (SQLException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      return 0;
+   }
 
-			int r = pstmt.executeUpdate();
+   // 항공 정보를 삭제하는 메소드
+   public int delALInfo(int ID) {
 
-			if (r > 0)
-				System.out.println(ID + " delete success");
-			else
-				System.out.println(ID + " delete failure");
+      try {
+         sql = "delete from airlineinfo where ID = ?";
+         pstmt = conn.prepareStatement(sql);
+         pstmt.setInt(1, ID);
 
-			return r;
+         int r = pstmt.executeUpdate();
 
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+         if (r > 0)
+            System.out.println(ID + " delete success");
+         else
+            System.out.println(ID + " delete failure");
 
-		return 0;
-	}
+         return r;
+
+      } catch (SQLException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+
+      return 0;
+   }
 }
